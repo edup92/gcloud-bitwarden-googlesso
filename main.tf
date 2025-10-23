5812ad9f91f90f9b6fa1022975866896a52e3295
edup92 <edup92mail@gmail.com>
2025-10-23 23:05:53 +0000

    up

--- main.tf
+++ main.tf
@@ -53,15 +53,7 @@ resource "google_compute_instance" "instance_bitwarden" {
     stack_type   = "IPV4_ONLY"
   }
   service_account {
-    email = "${data.google_project.current.number}-compute@developer.gserviceaccount.com"
-    scopes = [
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring.write",
-      "https://www.googleapis.com/auth/service.management.readonly",
-      "https://www.googleapis.com/auth/servicecontrol",
-      "https://www.googleapis.com/auth/trace.append"
-    ]
+    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
   }
   shielded_instance_config {
     enable_secure_boot          = false
