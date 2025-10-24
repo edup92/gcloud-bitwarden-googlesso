4413d912d38e7c62c80918738a015fe7ae372e4f
edup92 <edup92mail@gmail.com>
2025-10-24 01:19:58 +0000

    up

--- main.tf
+++ main.tf
@@ -221,7 +221,7 @@ data "google_dns_managed_zone" "zone_main" {
 }
 
 resource "google_dns_record_set" "a_record" {
-  name         = var.domain
+  name         = "${var.domain}."
   type         = "A"
   ttl          = 300
   managed_zone = data.google_dns_managed_zone.zone_main.name
