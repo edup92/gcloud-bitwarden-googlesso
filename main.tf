0d1c4219ae4cef8024a8229b07943ee177cc6dbf
edup92 <edup92mail@gmail.com>
2025-11-28 14:48:23 +0000

    up

--- main.tf
+++ main.tf
@@ -135,7 +135,7 @@ resource "google_compute_firewall" "fw_cf" {
     protocol = "tcp"
     ports    = ["443"]
   }
-  source_ranges = data.cloudflare_ip_ranges.cloudflare.ipv4_cidrs
+  source_ranges = data.cloudflare_ip_ranges.cloudflare.cidrs_ipv4
   target_tags   = [google_compute_instance.instance_bitwarden.name]
 }
 
